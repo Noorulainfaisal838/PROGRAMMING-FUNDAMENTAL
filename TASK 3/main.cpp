61ea730ff181672c31a0ca6d2c310c4bd543f156
Noorulainfaisal838 <faisalnoorulain7@gmail.com>
2024-10-23 22:56:01 +0500

    first commit

---
+++ TASK 3/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int rows = 4; // Number of rows
+    int cols = 6; //number of columns
+    for (int i = 1; i <= rows; ++i){
+        for (int j = 1; j <= cols; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+        }
+    
---
+++ TASK1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int i,j;
+    for (int i=0; i<=4; i++)
+    {
+    for (int j=1; j<=i; j++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+}
+    return 0;
+    }
